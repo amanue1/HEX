0a76593a04a2c0e9577fa41b74361266b34a24ad
Amanue1 <amanue1@protonmail.com>
2019-12-20 09:15:05 +0300

    created users app with login logout features, and made the first basic database schema

---
+++ users/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+
+from .forms import CustomUserCreationForm, CustomUserChangeForm
+from .models import CustomUser
+
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = CustomUser
+    list_display = ['email', 'username', 'first_name', 'last_name', 'gender', 'date_of_birth', 'department', 'why_join_HEX', ]
+
+admin.site.register(CustomUser, CustomUserAdmin)
+
